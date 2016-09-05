f4cafb19f4ca8b26a5a4954ecb50b7f7054108a4
dante <c.dante.federici@gmail.com>
2016-09-05 05:51:02 -0400

    First pass at routring and url

--- src/main.js
+++ src/main.js
@@ -3,43 +3,27 @@ import './main.scss';
 // Build the router
 import { Router5 } from 'router5';
 import { createLoader } from './util/systemLoader';
-
+import historyPlugin from 'router5-history';
 
 const route = (name, path, opts = {}) => ({ path, name, ...opts });
-
-const lodashUri = 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.15.0/lodash.min.js';
-const d3Uri = 'https://cdnjs.cloudflare.com/ajax/libs/d3/4.2.2/d3.min.js';
 
 // Define some routes to test
 const routes = [
 	// Resolve as module
-	route('root', '/', {
-		systemjs: d3Uri,
-	}),
-	// Resolve as keyed map
-	route('test', '/test', {
-		systemjs: {
-			lodash: lodashUri,
-			d3: d3Uri,
-		},
-	}),
-	// Resolve as module list
-	route('test2', '/test2', {
-		systemjs: [
-			lodashUri, d3Uri,
-		],
-	}),
-	route('fail', '/fail', {
-		systemjs: 'invalid module path!',
-	}),
+	route('root', '/'),
+	route('blog', '/blog'),
+	route('toys', '/toys'),
+	route('contact', '/contact'),
 ];
 
 const router = new Router5(routes, {
 	useHash: true,
+	hashPrefix: '!',
 	defaultRoute: 'root',
+	autoCleaUp: true,
 }).useMiddleware(createLoader({
 	routes,
-}));
+})).usePlugin(historyPlugin());
 
 // Build state from some defaults
 import { makeState, defaultMiddleware } from './util/state';
@@ -59,7 +43,6 @@ const rootReducer = combineReducers({
 // The State object
 const state = makeState(rootReducer, middleware);
 
-
 // Register ui component
 import ui from './ui';
 ui(state);
@@ -67,13 +50,4 @@ ui(state);
 
 // Kick off the router
 router.start();
-
-// Pass and get d3 as systemjs value
-router.navigate('test');
-
-// Pass and get object of { d3, lodash }
-// router.navigate('test2');
-
-// Fail
-// router.navigate('fail');
 
